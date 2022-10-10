25232e9a9c54b7fc3e782ccd655062fbac1a4ea5
Akbarov Jasurbek <akbarovjasurbek28@gmail.com>
2022-10-10 16:08:35 +0900

    Finished

---
+++ src/layout/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return(
+    <nav>
+      <div className="nav-wrapper header__nav #8d6e63 brown lighten-1">
+        <a href="#" className="brand-logo">React</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="#">Movie</a></li>
+        </ul>
+      </div>
+  </nav>
+  )
+}
